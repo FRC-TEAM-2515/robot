80c2a5c8682c0a6697a9b13be737cfb2f4347ad7
Kelly Reed Schuerman <kelly.schuerman@smsu.edu>
2022-02-13 10:17:30 -0600

    Trajectory Tracking
    previously unsaved files from prior to last commit

--- MrE/src/main/java/frc/robot/Robot.java
+++ MrE/src/main/java/frc/robot/Robot.java
@@ -14,7 +14,14 @@ package frc.robot;
 
 import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
 import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +39,9 @@ public class Robot extends TimedRobot {
 
     private RobotContainer m_robotContainer;
 
+    private String trajectoryBL1P1 = "paths/BL1P1.wpilib.json";
+
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -42,6 +52,14 @@ public class Robot extends TimedRobot {
         // autonomous chooser on the dashboard.
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
+        m_robotContainer.m_driveSubsystem.resetEncoders();
+        m_robotContainer.m_driveSubsystem.resetGyro();
+        try {
+            Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryBL1P1);
+            m_robotContainer.m_trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+         } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory: " + trajectoryBL1P1, ex.getStackTrace());
+         }
     }
 
     /**
