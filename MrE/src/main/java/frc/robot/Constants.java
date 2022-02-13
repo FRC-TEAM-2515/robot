349db86b280fd2eeafb40da486e4c006beaf75ca
Kelly Reed Schuerman <kelly.schuerman@smsu.edu>
2022-02-13 09:57:45 -0600

    Updated Drivetrain
    Added methods for trajectory tracking.

--- MrE/src/main/java/frc/robot/Constants.java
+++ MrE/src/main/java/frc/robot/Constants.java
@@ -8,25 +8,59 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package frc.robot;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be
+ * declared globally (i.e. public static). Do not put anything functional in
+ * this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
-   /**
-    * public static final class DriveConstants {
-    *   public static final int kLeftMotor1Port = 0;
-    *   public static final int kLeftMotor2Port = 1;
-    *   public static final int kRightMotor1Port = 2;
-    *   public static final int kRightMotor2Port = 3; 
-    * }
-    */ 
-}
+    public static final class DriveConstants {
 
+        public static final double kDeadband = 0.07;
+        public static final double kAcellerationLimit = 0.8; // Slew rate limiter
+        public static final double kSteeringOutputModifier = 0.5;
+        public static final double kWheelDiameterMeter = 0.1524;
+        public static final double kWheelDiameterInches = 6.0;
+        public static final double kDriveGearRatio = 10.71;
+        public static final double kEncoderTicksPerRevolution = 168; // 96 tested, actual from documentation is 42;
+        public static final double kEncoderTick2Feet = 1.0 / kEncoderTicksPerRevolution * kDriveGearRatio * kWheelDiameterInches * Math.PI / 12;
+        public static final double kEncoderTick2Meter = 1.0 / kEncoderTicksPerRevolution * kDriveGearRatio * kWheelDiameterMeter * Math.PI;
+
+        public static final double kEncoderPositionConversionFactor = kEncoderTick2Meter;
+        public static final double kEncoderVelocityConversionFactor = kDriveGearRatio * Math.PI * kWheelDiameterMeter / 60;
+
+        public static final double kAutoDriveForwardSpeed = 0.5;
+        public static final double kAutoDriveForwardDistance = 5;
+
+        // The Robot Characterization Toolsuite provides a convenient tool for obtaining these
+        // values for your robot.
+        public static final double ksVolts = 0.11653;
+        public static final double kvVoltSecondsPerMeter = 0.12895;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.016919;
+        // Example value only - as above, this must be tuned for your drive!
+        public static final double kPDriveVel = 0.5;
+
+        public static final double kTrackwidthMeters = 0.5588;
+        public static final DifferentialDriveKinematics kDriveKinematics =
+            new DifferentialDriveKinematics(kTrackwidthMeters);
+
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 1.5; 
+
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;
+    }
+
+}
